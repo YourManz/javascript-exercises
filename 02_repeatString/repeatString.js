dcb88a30e30345765ffb2f35287567d0f60e567b
Kobe Neilson <kobeneilson@gmail.com>
2023-10-18 13:51:54 -0700

    Completed 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTimes) {
+    let stringRepeat = '';
+    if (repeatTimes<0) {
+        return 'ERROR';
+    }
+    for (let i=0; i < repeatTimes; i++) {
+        stringRepeat += string;
+    };
+    return stringRepeat;
 };
 
 // Do not edit below this line
