4941fcfba5fd4d8c46a89cbcc47789caf51fa916
Kobe Neilson <kobeneilson@gmail.com>
2023-10-19 13:59:45 -0700

    completed 06-leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    if (leapYear % 4 === 0 && leapYear % 100 === 0) {
+        if (leapYear % 400 === 0) {
+            return true;
+        } else if (leapYear % 400 !== 0) {
+            return false;
+        }
+    } else if (leapYear % 4 === 0) {
+        return true;
+    } else if (leapYear % 4 !== 0) {
+        return false;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
