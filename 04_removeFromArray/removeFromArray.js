ac49d87244526fe00bc08b95f47e8abcd76996a2
Kobe Neilson <kobeneilson@gmail.com>
2023-10-19 13:19:01 -0700

    completed 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    elements.forEach((elmValue) => {
+        array.forEach((value, index) => {
+            if (value === elmValue) {
+                array.splice(index, 1);
+            };
+        });
+    })
 
+    return array;
 };
 
 // Do not edit below this line
