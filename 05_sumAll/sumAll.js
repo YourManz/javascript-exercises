fec2c95c40a09beadfa0ec99902919acead9f614
Kobe Neilson <kobeneilson@gmail.com>
2023-10-19 13:46:13 -0700

    completed 02-sumALl

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let sumArray = [];
+    if (num1<0 || num2<0) {
+        return "ERROR";
+    }
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
+    if (num1<num2) {
+        for (let i=num1; i<=num2; i++) {
+            sumArray.push(i);
+        };
+    } else if (num1>num2) {
+        for (let i=num1; i>=num2; i--) {
+            sumArray.push(i);
+        };
+    } else {
+        return "ERROR";
+    };
 
+    sumArray.forEach((value) => {
+        sum += value;
+    });
+    return sum;
 };
 
 // Do not edit below this line
