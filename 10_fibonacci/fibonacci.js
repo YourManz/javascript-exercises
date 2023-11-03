319cee056558fdc725455c7f3649be1aecb50857
Kobe Neilson <kobeneilson@gmail.com>
2023-11-03 13:08:29 -0700

    Completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthDigit) {
+    if (nthDigit <= 0) {
+        return 'OOPS';
+    }
+    let fibNumber = 1;
+    let fibArray = [1, 1, 2, 3];
+    for (let i = 0; i <= Number(nthDigit); i++) {
+        let newFib = fibArray[3+i] + fibArray[2+i]
+        fibArray.push(newFib);
+    };  
+    return fibArray[nthDigit-1];
 };
 
 // Do not edit below this line
